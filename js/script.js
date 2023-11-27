476a7d785757d50e7507e2d49c501f9c8329491f
kame-sama <ad.bodeev@gmail.com>
2023-11-27 16:03:33 +0500

    Add behavior to buttons

--- js/script.js
+++ js/script.js
@@ -1,13 +1,25 @@
-// let firstArgument = ;
-// let secondArgument = ;
-// let operator = ;
+let firstArgument = null;
+let secondArgument = null;
+let operator = null;
+let content = '';
 
 const display = document.querySelector('.display');
 const numbers = document.querySelector('.numbers');
+const operators = document.querySelector('.operators');
+const clearOrCompute = document.querySelector('.clear-or-compute');
 
 numbers.addEventListener('click', e => {
     populateDisplay(e);
 });
+
+operators.addEventListener('click', e => {
+    getOperator(e);
+});
+
+clearOrCompute.addEventListener('click', e => {
+    compute(e);
+    clear(e);
+})
 
 function add(firstArgument, secondArgument) {
     return +firstArgument + +secondArgument;
@@ -45,5 +57,44 @@ function operate(operator, firstArgument, secondArgument) {
 }
 
 function populateDisplay(button) {
-    display.textContent += button.target.textContent;
+    content += button.target.textContent;
+    display.textContent = content;
+}
+
+function getOperator(button) {
+    if (!operator) {
+        operator = button.target.textContent;
+    }
+    if (firstArgument === null) {
+        firstArgument = +content||+display.textContent;
+        content = '';
+    } else {
+        compute(button);
+        operator = button.target.textContent;
+    }
+}
+
+function compute(button) {
+    if (button.target.textContent != 'c') {
+        secondArgument = +content||+display.textContent;
+        console.log(firstArgument, operator, secondArgument);
+        display.textContent = operate(operator, firstArgument, secondArgument);
+        console.log(display.textContent);
+        content = '';
+        firstArgument =
+            button.target.textContent == '='
+            ? null
+            : display.textContent;
+        secondArgument = null;
+        operator = null;
+    }
+}
+
+function clear(button) {
+    if (button.target.textContent == 'c') {
+        display.textContent = '';
+        firstArgument = null;
+        secondArgument = null;
+        operator = null;
+    }
 }
